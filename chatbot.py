82e90fe5dfbc09e0de7d50068904654725b57744
arish420 <muhammadarish420@gmail.com>
2025-01-15 11:08:36 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,36 @@
 import streamlit as st
 import os
-
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+import getpass
+import os
+from PyPDF2 import PdfReader
+import sentence_transformers
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 ###############################################################################################
 files=os.listdir()
 ###############################################################################################
 files=[file for file in files if file.split(".")[-1]=="pdf"]
+###############################################################################################
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+text_data=get_pdf_text(files)
 
-st.write(files)
+st.write(text_data)
 
 st.title("Student Assistant")
