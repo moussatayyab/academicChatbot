d5a6bf523ddb9f0e25c2f3c4508d596a12d93beb
arish420 <muhammadarish420@gmail.com>
2025-01-15 11:04:37 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import os
 
-st.write(os.listdir())
+###############################################################################################
+files=os.listdir()
+###############################################################################################
+files=[file for file in files if file.split(".")[:-1]=="pdf"]
 
-
+st.write(files)
 
 st.title("Student Assistant")
