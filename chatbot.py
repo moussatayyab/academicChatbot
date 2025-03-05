eb2307165c690343c4f1f82ca3d58676ec128b6e
arish420 <muhammadarish420@gmail.com>
2025-03-05 15:44:57 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyPDFDirectoryLoader
@@ -25,9 +24,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.docstore.document import Document
 from datetime import datetime
-
-
-
+import gdown
 ###############################################################################################
 files=os.listdir()
 ###############################################################################################
@@ -42,8 +39,6 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 text_data=get_pdf_text(files[:1])
-
-
 
 
 # # split the extracted data into text chunks using the text_splitter, which splits the text based on the specified number of characters and overlap
@@ -79,15 +74,37 @@ vector_store = FAISS.from_documents(documents=documents, embedding=embeddings)
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vector_store.as_retriever()
 
+
+#############################################################################################################################
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
 from langchain_groq import ChatGroq
 
-llm = ChatGroq(
+llm_llama3 = ChatGroq(
     temperature=0,
-    model="mixtral-8x7b-32768",
+    model="llama-3.3-70b-versatile",
     api_key=GROQ_API_KEY
 )
+
+################################################################################################################################
+
+
+################################################################################################################################
+def download_db():
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, output_file, quiet=False)
+    return output_file
+k=""
+with open(download_db(),'r') as f:
+    f=f.read()
+    # st.write(f)
+    k=f
+    
+
+os.environ["OPENAI_API_KEY"] = k
+llm_openai = ChatOpenAI(model="gpt-4o-mini")
+######################################################################################################################################
+
 
 prompt = hub.pull("rlm/rag-prompt")
 
@@ -96,19 +113,40 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
-
 st.title("Student Assistant")
+
+selections=st.sidebar.selectbox("☰ Menu", ["Home","AI Assistant", "Feedback")
+
+
 query=""
 query=st.text_input("Write Query Here")
 
-res=""
+
+if selections=="Home":
+    st.markdown("""### **About the School Student Assistant Chatbot**  
+
+    The **School Student Assistant Chatbot** is an AI-powered virtual assistant designed to help students with their academic and school-related queries. It provides instant responses to common questions, assists with homework, shares important school updates, and offers guidance on schedules, subjects, and extracurricular activities.  
+    
+    This chatbot enhances student engagement by providing **quick, accurate, and interactive support** anytime, reducing the need for manual inquiries. It is built with advanced **natural language processing (NLP)** to understand student queries effectively and deliver relevant information in a conversational manner.  
+    
+    ### **Key Features:**  
+    ✅ **Homework Assistance** – Provides explanations and study resources.  
+    ✅ **Timetable & Schedule Support** – Helps students check class schedules.  
+    ✅ **School Announcements & Notices** – Delivers updates on events and policies.  
+    ✅ **Subject Guidance** – Answers subject-related queries.  
+    ✅ **Interactive & Voice Support** – Allows students to communicate via text or voice.  
+    
+    With its **user-friendly interface** and **AI-driven capabilities**, the chatbot enhances the learning experience by making information more accessible and helping students stay organized. """)
+
+
 if st.button("Submit") and query!="":
+    rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm_openai
+    | StrOutputParser()
+    )
+    st.subheader("OpenAI GPT Response")
     res=rag_chain.invoke(query)
     st.write(res)
 
@@ -117,20 +155,20 @@ if st.button("Submit") and query!="":
     st.write("")
     st.write("")
 
-    context=""
+    rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm_llama3
+    | StrOutputParser()
+    )
+    st.subheader("Meta Llama3 GPT Response")
+    res=rag_chain.invoke(query)
+    st.write(res)
 
-    for i in vector_store.similarity_search(query):
-        context += i.page_content 
+    # context=""
 
-if res!="":
-    # st.write(context)
-    with st.expander("Feedback"):
-        # Collect user feedback
-        rating = st.slider("Rate this response (1 = Bad, 5 = Excellent)", 1, 5, 3)
-        comment = st.text_area("Any additional feedback?")
-
-
-
+    # for i in vector_store.similarity_search(query):
+    #     context += i.page_content 
 
     # Get current date and time
     # now = datetime.now()
