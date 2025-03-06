24444e19554957ed3e36123ee1b3dae18392c23a
arish420 <muhammadarish420@gmail.com>
2025-03-06 13:12:52 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,6 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
@@ -36,8 +35,6 @@ from langchain.embeddings import OpenAIEmbeddings
 
 # Load environment variables
 load_dotenv()
-
-
 ###############################################################setting openai ai api##################################################################
 file_id = "1ug8pf1M1tes-CJMhS_sso372tvC4RQv8"
 output_file = "open_ai_key.txt"
@@ -96,59 +93,8 @@ vector_store = FAISS(
 # Set up retriever
 retriever = vector_store.as_retriever()
 
+##########################################################################setting groq api ###############################################################
 
-
-
-# files=os.listdir()
-# ###############################################################################################
-# files=[file for file in files if file.split(".")[-1]=="pdf"]
-
-###############################################################################################
-# def get_pdf_text(pdf_docs):
-#     text = ""
-#     for pdf in pdf_docs:
-#         pdf_reader = PdfReader(pdf)
-#         for page in pdf_reader.pages:
-#             text += page.extract_text()
-#     return text
-# text_data=get_pdf_text(files[:1])
-
-
-# # # split the extracted data into text chunks using the text_splitter, which splits the text based on the specified number of characters and overlap
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# text_chunks = text_splitter.split_text(text_data)
-# # print the number of chunks obtained
-# # len(text_chunks)
-
-
-
-# modelPath = "BAAI/bge-large-en-v1.5"
-
-# # Create a dictionary with model configuration options, specifying to use the CPU for computations
-# model_kwargs = {'device':'cpu'}
-# #if using apple m1/m2 -> use device : mps (this will use apple metal)
-
-# # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
-# encode_kwargs = {'normalize_embeddings': True}
-
-# # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
-# embeddings = HuggingFaceEmbeddings(
-#     model_name=modelPath,     # Provide the pre-trained model's path
-#     model_kwargs=model_kwargs, # Pass the model configuration options
-#     encode_kwargs=encode_kwargs # Pass the encoding options
-# )
-
-# # Convert text_chunks (list of strings) to Document objects
-# documents = [Document(page_content=chunk) for chunk in text_chunks]  
-
-# # Now use 'documents' instead of 'text_chunks'
-# vector_store = FAISS.from_documents(documents=documents, embedding=embeddings)
-
-# # Retrieve and generate using the relevant snippets of the blog.
-# retriever = vector_store.as_retriever()
-
-
-#############################################################################################################################
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
 from langchain_groq import ChatGroq
@@ -159,17 +105,16 @@ llm_llama3 = ChatGroq(
     api_key=GROQ_API_KEY
 )
 
-################################################################################################################################
+##############################################################setting opne ai llm ##################################################################
 
 llm_openai = ChatOpenAI(model="gpt-4o-mini")
-######################################################################################################################################
+###########################################################setting RAG document formatting ##############################################################
 
 
 prompt = hub.pull("rlm/rag-prompt")
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
-
-
+##############################################################setting prompts ###########################################################################################
 
 def get_feedback(feedback):
     return f"""
@@ -189,19 +134,30 @@ def get_feedback(feedback):
     - **Issues:** Issues Here
     """
 
+# Define a custom prompt template
+custom_prompt = PromptTemplate(
+    input_variables=["context", "question"],
+    template="""
+    You are an AI assistant answering user queries based on the provided context.
+    
+    Context:
+    {context}
+    
+    Question:
+    {question}
 
-
-
-
+    You can add additional information from your knowledgebase as well.
+    Provide a clear and concise response.
+    """
+    )
+############################################################################################################################################################
 
 
 st.title("Student Assistant")
-
 selections=st.sidebar.selectbox("☰ Menu", ["Home","AI Assistant", "Feedback"])
 
 
 query=""
-
 
 if selections=="Home":
     st.markdown("""The School Student Assistant Chatbot is an AI-powered virtual assistant designed to help students with their academic and school-related queries. It provides instant responses to common questions, assists with homework, shares important school updates, and offers guidance on schedules, subjects, and extracurricular activities.  
@@ -218,7 +174,7 @@ if selections=="AI Assistant":
     if st.button("Submit") and query!="":
         rag_chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
+        | custom_prompt
         | llm_openai
         | StrOutputParser()
         )
@@ -226,34 +182,20 @@ if selections=="AI Assistant":
         res=rag_chain.invoke(query)
         st.write(res)
     
-    #     # # performing a similarity search to fetch the most relevant context
-    #     st.write("")
-    #     st.write("")
-    #     st.write("")
+        # # performing a similarity search to fetch the most relevant context
+        st.write("")
+        st.write("")
+        st.write("")
     
-    #     rag_chain = (
-    #     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    #     | prompt
-    #     | llm_llama3
-    #     | StrOutputParser()
-    #     )
-    #     st.subheader("Meta Llama3 GPT Response")
-    #     res=rag_chain.invoke(query)
-    #     st.write(res)
-
-    # context=""
-
-    # for i in vector_store.similarity_search(query):
-    #     context += i.page_content 
-
-    # Get current date and time
-    # now = datetime.now()
-    # # Format as string
-    # formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
-    # pd.DataFrame({"DateTime":formatted_now,"Context":context,"AI Response":res,"User Feedback":""}.to_excel("user_feedback")
-                 
-# https://docs.google.com/spreadsheets/d/1ramLbRPuTHo4yY2ylTUIM1vyy539MumNGAdFCP5w9uY/edit?usp=sharing
-# https://docs.google.com/spreadsheets/d/1k1MYDZ7n9sIjPTfXFMFHMwEJOMkmhcWzikFVoXlH2SQ/edit?usp=sharing
+        rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | custom_prompt
+        | llm_llama3
+        | StrOutputParser()
+        )
+        st.subheader("Meta Llama3 GPT Response")
+        res=rag_chain.invoke(query)
+        st.write(res)
 
 if selections=="Feedback":
     
@@ -265,7 +207,6 @@ if selections=="Feedback":
     df=pd.read_csv(url)
     st.write(df.columns)
 
-    
 
     col1,col2=st.columns(2)
     with col1:
