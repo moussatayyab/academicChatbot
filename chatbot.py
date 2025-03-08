6de8f7218732422d6531a2bb9d4dcbc57358b9d2
arish420 <muhammadarish420@gmail.com>
2025-03-08 10:47:12 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -178,22 +178,10 @@ if selections=="AI Assistant":
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | custom_prompt
         | llm_openai
-        | StrOutputParser()
         )
         st.subheader("OpenAI GPT Response")
-        # res=rag_chain.invoke(query)
-        # st.write(res)
-
-        
-        with get_openai_callback() as cb:
-            res = rag_chain.invoke(query)
-            st.write(res)
-        
-            # Display token usage
-            st.write(f"Prompt Tokens: {cb.prompt_tokens}")
-            st.write(f"Completion Tokens: {cb.completion_tokens}")
-            st.write(f"Total Tokens: {cb.total_tokens}")
-            st.write(f"Total Cost (USD): {cb.total_cost:.6f}")
+        res=rag_chain.invoke(query)
+        st.write(res)
 
     
         # # performing a similarity search to fetch the most relevant context
@@ -208,7 +196,7 @@ if selections=="AI Assistant":
         )
         st.subheader("Meta Llama3 GPT Response")
         res=rag_chain.invoke(query)
-        st.write(type(res))
+        st.write(res)
       
         
 
