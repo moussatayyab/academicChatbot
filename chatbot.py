ae85b04b237a0878747da5cfa9cd9098387bf8b3
arish420 <muhammadarish420@gmail.com>
2025-03-08 12:33:21 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -203,7 +203,7 @@ if selections=="AI Assistant":
         st.subheader("Meta Llama3 GPT Response")
         res=rag_chain.invoke(query)
         st.write(res.content)
-        tokens["open_ai"]=res.response_metadata['token_usage']['total_tokens']
+        # tokens["open_ai"]=res.response_metadata['token_usage']['total_tokens']
         # tokens_df=pd.DataFrame(tokens.items())
         # tokens_df.to_csv("token_usage.csv")
         # st.write(tokens_df)
